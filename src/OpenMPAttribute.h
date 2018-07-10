28f5c477f44ae1abc79d444e927c3b0d3cab44d3
Yonghong Yan <yanyh15@gmail.com>
2018-07-10 14:42:04 -0400

    Update OpenMPAttribute.h
    add getter for the kind variable of the two classes. Remove the OpenNode class which is not the right design.

--- src/OpenMPAttribute.h
+++ src/OpenMPAttribute.h
@@ -20,6 +20,8 @@ class OpenMPClause {
     std::vector<const char*> lang_expr;
 
     void addLangExpr(const char * expr) { lang_expr.push_back(expr); }
+    
+    OpenMPClauseKind getKind() { return kind; }
 };
 
 class OpenMPDirective {
@@ -33,71 +35,10 @@ class OpenMPDirective {
 
     void addClause(OpenMPClause * clause) { clauses.push_back(clause);}
 
+    OpenMPDirectiveKind getKind() { return kind; }
+
     /* generate DOT representation of the directive */
     void generateDOT(const std::string * filename);
-
 };
-
-// standalone omp_parser
-class openMPNode {
-    
-    const char* type;
-    const char* id;
-    openMPNode* parent;
-    const char* val;
-    std::vector<openMPNode*> children;
-    
-    public:
-        
-        //openMPNode (char*);
-
-        std::vector<openMPNode*>* getChildren() {
-            return &children;
-        }
-
-        openMPNode* getLast() {
-            return children.back();
-        }
-
-        const char* getType() {
-            return type;
-        }
-
-        void setType (const char* nodeType) {
-            type = nodeType;
-        }
-
-        openMPNode* getParent() {
-            return parent;
-        }
-
-        void setParent (openMPNode* node) {
-            parent = node;
-        }
-
-        void addChild (openMPNode* node) {
-            //openMPNode* child = new openMPNode(childType);
-            //openMPNode* child = new openMPNode;
-            //child->setType(childType);
-            children.push_back(node);
-            node->setParent(this);
-            //return child;
-        }
-
-        const char* getVal() {
-            return val;
-        }
-
-        void setVal (const char* value) {
-            val = value;
-        }
-};
-
-/*
-openMPNode::openMPNode (char* nodeType) {
-    type = nodeType;
-};
-*/
-
 
 #endif //OMPPARSER_OPENMPATTRIBUTE_H_H
