707fb2ccf1127ebf2a2ce3d19aaf722cbf26a01c
World-Worst-Detector <42342410+World-Worst-Detector@users.noreply.github.com>
2019-02-28 18:45:14 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -19,11 +19,17 @@ int main( int argc, const char* argv[] ) {
 
     //const char* input = "omp parallel private (a[foo(x, goo(x, y)):100], b[1:30], c) num_threads (3*5+4/(7+10)) firstprivate (foo(x), y), shared (a, b, c[1:10]), copyin (a[foo(goo(x)):20],a,y) default (none) if (parallel : b) proc_bind (master) reduction (inscan, + : a, foo(x)) reduction (abc : x, y, z) allocate (omp_high_bw_mem_alloc : m, n[1:5]) allocate (no, allo, cator)";
 
+<<<<<<< HEAD
+	//const char* input = "omp parallel private (a[foo(x, goo(x, y)):100], b[1:30], c) firstprivate (foo(x), y), shared (a, b, c[1:10]) ";
+=======
 	const char* input = "OMP parallel private (a[foo(x, goo(x, y)):100], b[1:30], c) firstprivate (foo(x), y), shared (a, b, c[1:10]) ";
+>>>>>>> 08933e9f8f4560f67576979c19bbfb66841666e5
 
-	//const char* input = "omp parallel private (a[foo(x, goo(x, y)):100], b[1:30], c) num_threads (3*5+4/(7+10)) allocate (omp_user_defined_mem_alloc : m, n[1:5]) allocate (no, allo, cator)";
+	//const char* input = "omp parallel private (a[foo(x, goo(x, y)):100], b[1:30], c) num_threads (3*5+4/(7+10)) allocate (no, allo, cator)";
 	
-	
+        const char* input = "omp for collapse(a) order(dasfe) nowait ordered(sd)";
+   
+
     OpenMPDirective* openMPAST = parseOpenMP(input, NULL);
     
     output(openMPAST);
